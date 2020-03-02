70baff2029a4f704c4109caf87bba68f468dad7a
AndrewRobins51413 <h.andrew.robins@gmail.com>
2020-03-02 23:48:48 +0000

    Robins sgt nearly working.

--- features/SebastianAddGradeGradeBook.jsx
+++ features/SebastianAddGradeGradeBook.jsx
@@ -58,19 +58,21 @@ class AddGrade extends React.Component {
           <div className="input-group-prepend">
             <span className="input-group-text" id="inputGroup-sizing-default">{<i className="fas fa-user"></i>}</span>
           </div>
-          <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" value={this.state.name} placeholder="Name" onChange={this.handleNameChange}></input>
+          <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" alue={this.state.name} placeholder="Name" onChange={this.handleNameChange}></input>
         </div>
         <div className="input-group mb-3">
           <div className="input-group-prepend">
             <span className="input-group-text" id="inputGroup-sizing-default">{<i className="far fa-list-alt"></i>}</span>
           </div>
-          <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" value={this.state.course} placeholder="Course" onChange={this.handleCourseChange}></input>
+          <input type="text" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"
+            lue={this.state.course} placeholder="Course" onChange={this.handleCourseChange}></input>
         </div>
         <div className="input-group mb-3">
           <div className="input-group-prepend">
             <span className="input-group-text" id="inputGroup-sizing-default">{<i className="fas fa-graduation-cap"></i>}</span>
           </div>
-          <input type="number" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default" value={this.state.grade} placeholder="Grade" onChange={this.handleGradeChange}></input>
+          <input type="number" className="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default"
+            ue={this.state.grade} placeholder="Grade" onChange={this.handleGradeChange}></input>
         </div>
         <button className="btn btn-success mr-2" type="submit">Add</button>
         <button className="btn btn-secondary ml-2" type="reset">Cancel</button>
