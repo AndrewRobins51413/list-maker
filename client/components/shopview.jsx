98f99b6fbb91a790162dd7e7bc74498e46b03aab
AndrewRobins51413 <h.andrew.robins@gmail.com>
2020-04-24 20:39:24 +0000

    Struggling with React

--- client/components/shopview.jsx
+++ client/components/shopview.jsx
@@ -1,19 +1,46 @@
 import React from 'react';
+import DateTimePicker from 'react-datetime-picker';
 // import GradeTable from './grade-table';
 
 class ShopView extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      view: ''
+      date: '',
+      time: '',
+      grades: []
     };
-    // this.shopView = this.shopView.bind(this);
+    this.shopView = this.shopView.bind(this);
+  }
+
+  shopView() {
+    var dateTime = new Date();
+    dateTime.setHours(dateTime.getHours(), dateTime.getMinutes() +1,0,0);
+    this.setState(
+      date : dateTime,
+      time:dateTime.toLocaleTimeString()
+    );
   }
 
   render() {
 
     return (
-      <h1> Shopview3 </h1>
+      <div>
+        <table className=" table table-success table-striped">
+          <thead className="thead-light col-md-8 col-sm-8 ">
+            <tr>
+              <th>Item</th>
+              <th>Quantity</th>
+              <th>Note</th>
+              <th>Got It?</th>
+            </tr>
+          </thead>
+          <tbody>{this.props.grades}</tbody>
+        </table>
+        <button >
+          time
+        </button>
+      </div>
     );
   }
 }
