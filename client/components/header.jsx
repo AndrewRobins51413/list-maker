807378cb6555e95c117c3b070cacebb63020326b
AndrewRobins51413 <h.andrew.robins@gmail.com>
2020-05-03 21:23:54 +0000

    added header file

---
+++ client/components/header.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+import HomeView from './homeview';
+import ShopView from './shopview';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    const titleStyle = {
+      color: 'darkgreen',
+      fontStyle: 'italic',
+      backgroundColor: 'lightgrey'
+    };
+    return (
+      <>
+        <Router>
+          <div>
+            <nav>
+              <div className="row d-flex">
+                <ul>
+                  <Link to="/homeview"><h2 style={titleStyle}>HomeView</h2></Link>
+                </ul>
+                <ul>
+                  <Link to="/shopview"><h2 style={titleStyle}>ShopView</h2></Link>
+                </ul>
+              </div>
+            </nav>
+            <Switch>
+              <Route exact path='/shopview'
+                render={props =>
+                  <ShopView
+                    {...props} />} />
+
+              <Route exact path='/homeview'
+                render={props =>
+                  <HomeView
+                    {...props} />} />
+
+            </Switch>
+          </div>
+        </Router>
+      </>
+    );
+  }
+}
+export default Header;
