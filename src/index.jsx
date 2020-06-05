c27e46f376cfc59db986d0df9ea1c7d3d1456fb8
AndrewRobins51413 <h.andrew.robins@gmail.com>
2020-06-05 19:58:41 +0000

    React environment attempt 12

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function CustomButton(){
+    <div>Hello?
+      <button>ClickMe</button>
+    </div>
+}
+
+ReactDOM.render(<CustomButton />, document.getElementById('root'));
